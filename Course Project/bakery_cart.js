07bbff98aa2ce67995a4018a1ea419493c2511e7
JohnMcNeillFSCJ <141746060+JohnMcNeillFSCJ@users.noreply.github.com>
2023-12-11 06:16:32 -0500

    Add files via upload

--- Course Project/bakery_cart.js
+++ Course Project/bakery_cart.js
@@ -1,4 +1,103 @@
+"use strict";
+
 /*
 * Author: John McNeill
 * Date: 12/10/23
 */
+
+
+
+window.addEventListener("load", setupCart);
+
+function setupCart() 
+{
+    var addButtons = document.getElementsByClassName("addButton");
+
+    for (var i = 0; i < addButtons.length; i++)
+    {
+        addButtons[i].onclick = addItem;
+    }
+}
+
+function addItem(e) 
+{
+    //description of the food item is the next sibling element to the 
+    //button that was clicked by the customer. 
+    //Use the next Element Sibling property to reference the next sibling
+    //element to the target of the event object. 
+    //Store the sibling element in a variable
+    var foodItem = e.target.nextElementSibling;
+    //console.log(foodItem);
+
+    var foodID = foodItem.id;
+
+    //console.log(foodID);
+
+    //use cloneNode() to create a copy of the foodItem element and all of 
+    //its descendants. Store the clone node in a variable
+    var foodDescription = foodItem.cloneNode(true);
+    //console.log(foodDescription.firstElementChild); 
+
+    var cartBox = document.getElementById("cart");
+
+    //console.log(cartBox.id);
+
+    //The shopping cart needs to determine whether a product ordered by the customer
+    //has already been ordered. To do this, add a span element to the top
+    //of each item in the cart containing the number of items of each product ordered
+    //and update that value when a product order is repeated. 
+
+    var duplicateOrder = false;
+
+    //console.log(cartBox.childElementCount);
+
+    for (var i = 0; i < cartBox.childElementCount; i++)
+    {
+
+        //console.log("Test");
+
+        //determine whether the ID of the element node equals foodID.
+        if (cartBox.children[i].id == foodID)
+        {
+
+            //console.log("Test");
+
+            //if so, the customer has previously placed that menu item 
+            //in the cart. 
+
+            duplicateOrder = true;
+
+            //Increate the value of the first element child node by 1 to 
+            //indicate an additional order and then break out of the for loop
+            cartBox.children[i].firstChild.textContent++;
+
+            //console.log(cartBox.children[i].firstChild.textContent); 
+
+            break;
+        }
+    }
+
+    //test whether duplicateOrder is still false
+    if (duplicateOrder === false)
+    {
+        //console.log("Here!");
+
+        //if so, create a variable named orderCount storing a span element node
+        var orderCount = document.createElement("span");
+
+        //set the text content of the orderCount element to 1
+        orderCount.textContent = 1;
+
+        //insert orderCount as the first child of the foodDescription node
+        //structure 
+        foodDescription.insertBefore(orderCount, foodDescription.firstChild);
+
+        //append foodDescription to cartBox as a new productOrder
+        cartBox.appendChild(foodDescription);
+
+        //console.log(orderCount.textContent);
+
+    }
+
+    
+}
--- Course Project/bakery_credit.js
+++ Course Project/bakery_credit.js
@@ -1,4 +1,193 @@
+"use strict"; 
+
 /*
 * Author: John McNeill
 * Date: 12/10/23
 */
+
+
+window.addEventListener("load", function ()
+{
+    //retrieve the field/value pairs from the URL
+    var orderData = location.search;
+    //console.log("location = " + orderData);
+    //slice the orderData text string to remove the first '?' char,
+    //replace every occurence of the '+' character with a blank space,
+    //and decode the URI-encoded characters
+    orderData = orderData.slice(orderData.indexOf('?'));
+    orderData = orderData.replace(/\+/g, ' ');
+    orderData = decodeURIComponent(orderData);
+
+    //split the orderData variable at every occurence of a '&' or '=' character
+    //and store the substrings in the orderFields array variable
+    var orderFields = orderData.split(/[&=]/g);
+
+    //assign data from orderFields into corresponding fields in the order form
+    document.getElementById("order").elements.modelName.value = orderFields[3];
+    document.getElementById("order").elements.modelQty.value = orderFields[5];
+    document.getElementById("order").elements.orderCost.value = orderFields[7];
+    document.getElementById("order").elements.subTotal.value = orderFields[15];
+    document.getElementById("order").elements.salesTax.value = orderFields[17];
+    document.getElementById("order").elements.cartTotal.value = orderFields[19];
+
+    //console.log(typeof document.getElementById("order").elements.salesTax.value)
+
+});
+
+//another eventListener for the window load event that runs different validation 
+//event handlers when the page is loaded by the browser. 
+window.addEventListener("load", function () 
+{
+
+    //run the runSubmit() function when the subButton is clicked. 
+    document.getElementById("subButton").onclick = runSubmit;
+
+    //run the validateName() function when a value is input into the cardHolder field
+    document.getElementById("cardHolder").oninput = validateName;
+
+    //run the validateNumber() function when a value is input into the cardNumber field
+    document.getElementById("cardNumber").oninput = validateNumber;
+
+    //run the validateDate() function when a value is input into the expDate field. 
+    document.getElementById("expDate").oninput = validateDate;
+
+    //run the validateCVC() function when a value is input into the cvc field. 
+    document.getElementById("cvc").oninput = validateCVC;
+
+});
+
+//this function runs when the form is submitted
+function runSubmit() 
+{
+
+    validateName();
+    validateCredit();
+    validateNumber();
+    validateDate();
+    validateCVC();
+
+}
+
+//the purpose of this function is to validate the credit card expiration date 
+//stored in the expDate field
+function validateDate() 
+{
+    var expDate = document.getElementById("expDate");
+
+    //if no value has been entered for the expiration date
+    if (expDate.validity.valueMissing)
+    {
+        //set the custom validation message 
+        expDate.setCustomValidity("Enter the expiration date");
+    }
+    //if the expiration date does not match the regular expression pattern:
+    else if (/^(0[1-9]|1[0-2])\/20[12]\d$/.test(expDate.value) === false)
+    {
+        //set the custom validation message
+        expDate.setCustomValidity("Enter a valid expiration date");
+    }
+    else
+    {
+        //otherwise, set the custom validation message to an empty text string
+        expDate.setCustomValidity("");
+    }
+}
+
+
+/* Functions already provided in the file */
+
+function validateName()
+{
+    var cardName = document.getElementById("cardHolder");
+    if (cardName.validity.valueMissing)
+    {
+        cardName.setCustomValidity("Enter the card holder");
+    } else
+    {
+        cardName.setCustomValidity("");
+    }
+}
+
+
+function validateCredit()
+{
+    var creditCard = document.forms.credit.elements.company[0];
+    if (creditCard.validity.valueMissing)
+    {
+        creditCard.setCustomValidity("Select your credit card");
+    } else
+    {
+        creditCard.setCustomValidity("");
+    }
+}
+
+function validateNumber()
+{
+    var cardNumber = document.getElementById("cardNumber");
+    if (cardNumber.validity.valueMissing)
+    {
+        cardNumber.setCustomValidity("Enter your card number");
+    } else if (cardNumber.validity.patternMismatch)
+    {
+        cardNumber.setCustomValidity("Enter a valid card number");
+    } else if (luhn(cardNumber.value) === false)
+    {
+        cardNumber.setCustomValidity("Enter a legitimate card number");
+    } else
+    {
+        cardNumber.setCustomValidity("");
+    }
+}
+
+function validateCVC()
+{
+
+    var cardCVC = document.getElementById("cvc");
+    var creditCard = document.querySelector('input[name="company"]:checked').value;
+
+    if (cardCVC.validity.valueMissing)
+    {
+        cardCVC.setCustomValidity("Enter your code CVC number");
+    } else if ((creditCard === "amex") && (/^\d{4}$/.test(cardCVC.value) === false))
+    {
+        cardCVC.setCustomValidity("Enter a 4-digit CVC number");
+    } else if ((creditCard !== "amex") && (/^\d{3}$/.test(cardCVC.value) === false))
+    {
+        cardCVC.setCustomValidity("Enter a 3-digit CVC number");
+    } else
+    {
+        cardCVC.setCustomValidity("");
+    }
+
+
+}
+
+function sumDigits(numStr)
+{
+    var digitTotal = 0;
+    for (var i = 0; i < numStr.length; i++)
+    {
+        digitTotal += parseInt(numStr.charAt(i));
+    }
+    return digitTotal;
+}
+
+function luhn(idNum)
+{
+    var string1 = "";
+    var string2 = "";
+
+    // Retrieve the odd-numbered digits
+    for (var i = idNum.length - 1; i >= 0; i -= 2)
+    {
+        string1 += idNum.charAt(i);
+    }
+    // Retrieve the even-numbered digits and double them
+    for (var i = idNum.length - 2; i >= 0; i -= 2)
+    {
+        string2 += 2 * idNum.charAt(i);
+    }
+
+    // Return whether the sum of the digits is divisible by 10
+    return sumDigits(string1 + string2) % 10 === 0;
+}
